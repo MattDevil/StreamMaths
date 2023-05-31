488ae117f9775b9ea602a26dabfc066a911a432c
Matthieu Devillers <matthieu.devillers@ac-rennes.fr>
2023-05-31 23:20:40 +0200

    move 1

--- start.py
+++ start.py
@@ -1,21 +1,33 @@
 from math import sqrt
-from handcalcs.handcalcs import handcalc
+from handcalcs import handcalc
 import streamlit as st
 
 @handcalc
+
 def quadratic(a,b,c):
     x_1 = (-b + sqrt(b**2 - 4*a*c)) / (2*a)
     x_2 = (-b - sqrt(b**2 - 4*a*c)) / (2*a)
-    return locals()
+    return [x_1,x_2]
 
 a = st.slider("Value for a:", 1,5, 5)
 b = st.slider("Value for b:", -10, 10, -5)
 c = st.slider("Value for c:", -20,0, -5)
 
 st.write("Quadratic equation in x:")
-st.latex(f"{a}x^2 + {b}x + {c} = 0")
+if b<0:
+    text1=" -{b}x"
+else :
+    text1=" +{b}x"
+if c<0:
+    text2=" -{c}"
+else:
+    text2=" -{c}"
+    
+text="{a}x^2"+text1+text2+" =0"
+st.latex(text)
 
-latex_code, vals = quadratic(a,b,c)
-st.latex(latex_code)
+%latex_code
+vals = quadratic(a,b,c)
+%st.latex(latex_code)
 st.write("Vals from returned dict:")
 st.write("x_1:", vals["x_1"], "x_2:", vals["x_2"])
